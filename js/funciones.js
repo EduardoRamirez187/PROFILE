2a2648113f57bd437461813a7d39c90d0a71283a
Eduardo Ramirez <edualf55@gmail.com>
2025-03-10 20:06:33 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,9 @@
+// Mostrar una alerta de bienvenida
+document.addEventListener("DOMContentLoaded", function () {
+    alert("¡Bienvenido a mi currículum!");
+});
+
+// Alternar entre temas claros y oscuros
+function toggleTheme() {
+    document.body.classList.toggle("dark-theme");
+}
